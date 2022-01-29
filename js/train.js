77720a00f2300cea9cd3a579dc63d38fdfa25ce8
RomanZinoviev <rzinovijev@gmail.com>
2022-01-29 14:58:06 +0200

    jh

--- js/train.js
+++ js/train.js
@@ -175,47 +175,163 @@
 // };
 // console.log(getTotalFriendCount(users))
 
-const friends = [
-    { name: "Anna", books: ["Bible", "Harry Potter"], age: 21 },
-    { name: "Bob", books: ["War and peace", "Romeo and Juliet"], age: 26 },
-    { name: "Alice", books: ["War and peace", "Romeo and Juliet"]},
-    { name: "Oleksii", books: ["Bible","War and peace","Harry Potter",  "Romeo and Juliet"], age: 26},
-]
+// const friends = [
+//     { name: "Anna", books: ["Bible", "Harry Potter"], age: 21 },
+//     { name: "Bob", books: ["War and peace", "Romeo and Juliet"], age: 26 },
+//     { name: "Alice", books: ["War and peace", "Romeo and Juliet"]},
+//     { name: "Oleksii", books: ["Bible","War and peace","Harry Potter",  "Romeo and Juliet"], age: 26},
+
 // Task-2: Порахувати кількість книжок користувачів, у яких є книга "Harry Potter",не використовуючи метод includes.
 // (перевірити користувачів на наявність потрібної книги за допомогою indexOf(const searchBook = "Harry Potter"
 
 
-const searchBook = "Harry Potter"
+// const searchBook = "Harry Potter"
 
-const totalBooks = friends.reduce((acc, friend) => {
-  // console.log(!!~friend.books.indexOf(searchBook))
-  if (!!~friend.books.indexOf(searchBook)) {
-   return acc+friend.books.length
-  }
-  return acc+0
-},0)
-console.log(totalBooks)
+// const totalBooks = friends.reduce((acc, friend) => {
+//   // console.log(!!~friend.books.indexOf(searchBook))
+//   if (!!~friend.books.indexOf(searchBook)) {
+//    return acc+friend.books.length
+//   }
+//   return acc+0
+// },0)
+// console.log(totalBooks)
 
-const cars = [
-  { brand: "Audi", cost: 11000 },
-  { brand: "BMW", cost: 9000 },
-  { brand: "Honda", cost: 8000 },
-  { brand: "Tesla", cost: 40000 },
-  { brand: "VW", cost: 7000 },
-];
+// const cars = [
+//   { brand: "Audi", cost: 11000 },
+//   { brand: "BMW", cost: 9000 },
+//   { brand: "Honda", cost: 8000 },
+//   { brand: "Tesla", cost: 40000 },
+//   { brand: "VW", cost: 7000 },
+// ];
 
-const minValue = Number(prompt("From"));
-let maxValue = Number(prompt("To"));
-console.log(minValue);
+// const minValue = Number(prompt("From"));
+// let maxValue = Number(prompt("To"));
+// console.log(minValue);
 
-if (maxValue === 0) {
-    maxValue = Infinity;
-}
-console.log(maxValue);
-const filterCars = cars.filter(car => {
-    if (car.cost >= minValue && car.cost <= maxValue) {
-        return car;
+// if (maxValue === 0) {
+//     maxValue = Infinity;
+// }
+// console.log(maxValue);
+// const filterCars = cars.filter(car => {
+//     if (car.cost >= minValue && car.cost <= maxValue) {
+//         return car;
+//     }
+
+// })
+// console.log(filterCars);
+
+// const bank = {
+//   name: 'Jordj',
+//   cardname:' Privat',
+//   total: 3600,
+//   totalSum() {
+//     console.log(`${this.name} , balance: ${this.total}`);
+//   },
+//   add(enterSum) {
+//     this.total += enterSum;
+//     bank.totalSum.call(this);
+//   },
+//   writeOf(escapeSum) {
+//     this.total -= escapeSum;
+//     bank.totalSum.call(this);
+//   },
+// };
+
+// bank.totalSum ();
+
+
+// const Den = {
+//     name: 'Den',
+//     cardName: 'Monobank',
+//     total: 10000,
+// };
+
+// bank.totalSum.call(Den);
+
+// bank.add(1000);
+
+// bank.add.apply(Den, [5000]);
+
+// const escapeMoney = bank.writeOf.bind(Den);
+// escapeMoney(4000);
+
+const products = [ {
+    id : 'sku1',
+    qty: 1,
+}, {
+    id : 'sku2',
+    qty: 2,
+}, {
+    id : 'sku3',
+    qty: 3,
+}, {
+    id : 'sku1',
+    qty: 6,
+}, {
+    id : 'sku1',
+    qty: 8,
+}, {
+    id : 'sku2',
+    qty: 19,
+}, {
+    id : 'sku4',
+    qty: 1,
+}]
+
+// products.forEach((product, i) => {
+//   if(product[i].id === product[i + 1].id) {
+//     product[i].qty + product[i + 1].qty;
+//     products.slice(i);
+//   }
+//   return products
+// })
+
+const removeSameProdact = function (products) {
+  for (let i = 0; i < products.length; i++) {
+    for (let j = i + 1; j < products.length; j++) {
+      if (products[i].id == products[j].id) {
+        products[i].qty += products[j].qty;
+        products.splice(j, 1);
+        j-=1
+      }
     }
+  }
+  console.log(products)
+  
+};
+removeSameProdact(products)
 
-})
-console.log(filterCars);
+const hogvarts = {
+    griffindor: [{
+            name: "Harry",
+            points: 15
+        },
+        {
+            name: "Hermiona",
+            points: 16
+        },
+        {
+            name: "Ron",
+            points: 14
+        },
+    ],
+    sliserin: [{
+            name: "Draco",
+            points: 25
+        },
+        {
+            name: "Goyl",
+            points: 40
+        },
+        {
+            name: "Crabbe",
+            points: 5
+        },
+    ]
+}
+//задача 3 вывести всех студентов. которые учатся в гриф. или  в слизарене
+//динамически добавлять тотал сум слизарен и гриффендор
+hogvarts.allStudents('griffindor')
+hogvarts.allStudents('sliserin')
+hogvarts.allPoints('sliserin')
+
